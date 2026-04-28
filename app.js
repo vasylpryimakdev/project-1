23302025bc6c358e898ea49ec0b8d6cf9d91df10
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-28 16:31:16 +0300

    Add command parsing for file delete, rename, and append actions

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const fs = require("fs/promises");
 
 (async () => {
+  // commands
+  const CREATE_FILE = "create a file";
+  const DELETE_FILE = "delete the file";
+  const RENAME_FILE = "rename the file";
+  const ADD_TO_FILE = "add to the file";
+
   const createFile = async (path) => {
     try {
-      let existingFileHandle = await fs.open(path, "r");
-
+      const existingFileHandle = await fs.open(path, "r");
       existingFileHandle.close();
 
       return console.log(`The file ${path} already exists.`);
@@ -15,8 +20,18 @@ const fs = require("fs/promises");
     }
   };
 
-  //commands
-  const CREATE_FLIE = "create a file";
+  const deleteFile = async (path) => {
+    console.log(`Deleting ${path}...`);
+  };
+
+  const renameFile = async (oldPath, newPath) => {
+    console.log(`Renaming ${oldPath} to ${newPath}...`);
+  };
+
+  const addToFile = async (path, content) => {
+    console.log(`Adding to ${path}...`);
+    console.log(`Content: to ${content}...`);
+  };
 
   const commandFileHandler = await fs.open("./command.txt", "r");
 
@@ -31,14 +46,34 @@ const fs = require("fs/promises");
 
     const command = buff.toString("utf-8");
 
-    if (command.includes(CREATE_FLIE)) {
-      const filePath = command.substring(CREATE_FLIE.length + 1);
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE.length + 1);
       createFile(filePath);
+    }
+
+    if (command.includes(DELETE_FILE)) {
+      const filePath = command.substring(DELETE_FILE.length + 1);
+      deleteFile(filePath);
+    }
+
+    if (command.includes(RENAME_FILE)) {
+      const _idx = command.indexOf(" to ");
+      const oldFilePath = command.substring(RENAME_FILE.length + 1, _idx);
+      const newFilePath = command.substring(_idx + 4);
+
+      renameFile(oldFilePath, newFilePath);
+    }
+
+    if (command.includes(ADD_TO_FILE)) {
+      const _idx = command.indexOf(" this content: ");
+      const filePath = command.substring(ADD_TO_FILE.length + 1, _idx);
+      const content = command.substring(_idx + 15);
+
+      addToFile(filePath, content);
     }
   });
 
   const watcher = fs.watch("./command.txt");
-
   for await (const event of watcher) {
     if (event.eventType === "change") {
       commandFileHandler.emit("change");
