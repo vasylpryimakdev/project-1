789cc2e497a3eb9028f638eee10737f2657bc54b
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-28 15:34:08 +0300

    Add create file command handler with dynamic file creation

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const createFile = async (path) => {
+    try {
+      let existingFileHandle = await fs.open(path, "r");
+
+      existingFileHandle.close();
+
+      return console.log(`The file ${path} already exists.`);
+    } catch (e) {
+      const newFileHandle = await fs.open(path, "w");
+      console.log("A new file was successfully created.");
+      newFileHandle.close();
+    }
+  };
+
+  //commands
+  const CREATE_FLIE = "create a file";
+
   const commandFileHandler = await fs.open("./command.txt", "r");
 
-  commandFileHandler.on("change", () => {
-    commandFileHandler.emit("change", async () => {
-      const size = (await commandFileHandler.stat()).size;
-      const buff = Buffer.alloc(size);
-      const offset = 0;
-      const length = buff.byteLength;
-      const position = 0;
+  commandFileHandler.on("change", async () => {
+    const size = (await commandFileHandler.stat()).size;
+    const buff = Buffer.alloc(size);
+    const offset = 0;
+    const length = buff.byteLength;
+    const position = 0;
 
-      await commandFileHandler.read(buff, offset, length, position);
+    await commandFileHandler.read(buff, offset, length, position);
 
-      console.log(buff.toString("utf-8"));
-    });
+    const command = buff.toString("utf-8");
+
+    if (command.includes(CREATE_FLIE)) {
+      const filePath = command.substring(CREATE_FLIE.length + 1);
+      createFile(filePath);
+    }
   });
 
   const watcher = fs.watch("./command.txt");
